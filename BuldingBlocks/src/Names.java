9d2bcdcd620259aa2eff99bef68b50546b9ad035
Benjamin Avila <badjr23@hotmail.com>
2026-03-30 01:12:01 -0600

    4-the-main-method

---
+++ BuldingBlocks/src/Names.java
@@ -0,0 +1,7 @@
+public class Names {
+
+    public static void main(String[] args) {
+        System.out.println("First name: " + args[0]);
+        System.out.println("Last Name: " + args[1]);
+    }
+}
---
+++ BuldingBlocks/src/PermissiblesWaysMainMethod.java
@@ -0,0 +1,27 @@
+public class PermissiblesWaysMainMethod {
+
+//    public static void main(String[] args){
+//
+//        }
+
+//    public static void main(String[] someOtherSillyName){
+//
+//    }
+
+//    public static void main(String args[]){
+//
+//    }
+
+//    public static void main(String...args){
+//
+//    }
+
+//    public final static void main(final String[] args){
+//
+//    }
+
+    static public void main(String[] args) {
+
+    }
+
+}
