7028e28f5ee81546335b198a57a973d2362183db
Benjamin Avila <badjr23@hotmail.com>
2026-04-16 23:49:23 -0600

    9-text-blocks

---
+++ BuldingBlocks/src/TextBlocks.java
@@ -0,0 +1,18 @@
+public class TextBlocks {
+
+    public static void main(String[] args) {
+//        String title = """
+//                "Java SE 17 Developer Couser"
+//                    by Luka Popov""";
+//        System.out.println(title + "Hello!");
+
+        String title = """
+                "Java SE 17 Developer Couser"
+                    by Luka Popov
+        """;
+        System.out.println(title + "Hello!");
+
+    }
+
+
+}
