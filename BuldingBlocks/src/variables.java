e77c63f6caae69fdb5ab5c5047b2bd17cee67796
Benjamin Avila <badjr23@hotmail.com>
2026-04-24 00:44:43 -0600

    10-variables

---
+++ BuldingBlocks/src/variables.java
@@ -0,0 +1,19 @@
+public class variables {
+
+    // multiple variables can be declared and/or initialozed
+    // in a single line (bad practice, but it compiles)
+
+    public static void main(String[] args){
+        int x, y;
+        String firstName = "John", lastName = "Wayne";
+        boolean v = true, w, z= false;
+
+        //you cannot declare variables of different ype in a single line
+        //int x, String name; // DOES NOT COMPILE
+
+        // "single line" means within the same command e.g.
+//        int x,
+//                String name; // "same line", does not compile
+//
+    }
+}
